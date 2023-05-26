de47e038f1c317ef5636481abf169716d4cf243f
900529 <hijh95@gmail.com>
2023-05-26 18:00:45 +0900

    0526/18:00

--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -1,5 +1,21 @@
 package com.javaex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.service.GuestbookService;
+
 public class GuestbookController {
-	
+
+	@Autowired
+	private GuestbookService guestbookService;
+
+	// 방명록폼 addList
+	@RequestMapping(value = "/guestbook/addList",method= {RequestMethod.GET,RequestMethod.POST})
+	public void addList() {
+		System.out.println("GuestbookController.addList()");
+	}
 }
+
+
